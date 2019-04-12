298531998ad23da0e6ac8f13596a9cb837ee4e89
Brettyoke49 <Bmprobe@g.clemson.edu>
2019-04-12 19:54:42 -0400

    Added looping with increasing transmission number, TODO: measure latency

--- client.py
+++ client.py
@@ -1,12 +1,25 @@
 import socket
+import signal
+import sys
+
+def signal_handler(sig, frame):
+        print('Client Terminating...')
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+PORT = 9099        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    msg = "Hello World"
-    s.sendall(msg.encode())
-    data = s.recv(1024).decode()
+
+transmission = str(0);
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST, PORT))
+    while True:
+      sock.sendall(transmission.encode())
+      transmission = sock.recv(1024).decode()
+      transmission = str(int(transmission) + 1)
+      print("From server: ", transmission)
 
 print('Received', repr(data))
--- server.py
+++ server.py
@@ -1,17 +1,17 @@
 import socket
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+PORT = 9099        # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind((HOST, PORT))
+    sock.listen()
+    conn, addr = sock.accept()
     with conn:
-        print('Connected by', addr)
+        print('Connected to: ', addr)
         while True:
-            data = conn.recv(1024).decode()
-            print(data)
-            if not data:
-                break
-            conn.sendall(data.encode())
+          data = conn.recv(1024).decode()
+          data = str(int(data) + 1)
+          print("From client: ", data)
+          if not data: break
+          conn.sendall(data.encode())
