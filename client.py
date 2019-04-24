db0153a2fde79d1e7ecc25a8188fb8b97a1ec95a
Brettyoke49 <Bmprobe@g.clemson.edu>
2019-04-23 21:35:24 -0400

    Added server summary statistics and fixed crash when received invalid int literal, added client timers and base for DoS mode. Need to fix traceroute call in server.

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import signal
 import sys
+import datetime
 
 def signal_handler(sig, frame):
         sock.sendall(str(-1).encode())
@@ -10,18 +11,59 @@ def signal_handler(sig, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 9099        # The port used by the server
+HOST = '127.0.0.1'  # The server's default IP address
+PORT = 9099        # The default port used by the server
+NormDuration = 60 # Default time for normal measurements
+DosDuration = 60 # Default time for DoS measurements
 
+if(len(sys.argv) >= 2 and str(sys.argv[1]) == "-h") : # Help option
+  print("Usage: python3 client.py [Normal_Duration] [DoS_Duration] [HOST] [PORT]")
+  print("Set Durations in seconds, default is 60 seconds each")
+  print("Default HOST is localhost (127.0.0.1), default PORT is 9099")
+  print("Durations must be set together or not at all, same with HOST/PORT")
+  exit()
 
+if(len(sys.argv) >= 3) : # Capturing arguments
+  NormDuration = int(sys.argv[1])
+  DosDuration = int(sys.argv[2])
+  if(len(sys.argv) >= 5) :
+    HOST = sys.argv[3]
+    PORT = int(sys.argv[4])
+
+feedbackInterval = NormDuration / 10
 transmission = str(0);
+intervals = 1;
+DoSing = False;
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
-    while True:
-      sock.sendall(transmission.encode())
+    sock.connect((HOST, int(PORT)))
+    print("If you must end the program early, use Ctrl-C on the CLIENT (this) program. NOT the server!")
+    start = datetime.datetime.now()
+
+    while True: # Main transmission loop
+      sock.sendall(transmission.encode()) # Send/Receive
       transmission = sock.recv(1024).decode()
       transmission = str(int(transmission) + 1)
-      print("From server: ", transmission)
+
+      if(int(transmission) % 10 == 0) : # Check for necessary updates
+        timeElapsed = datetime.datetime.now() - start
+        if(timeElapsed.total_seconds() > feedbackInterval) :
+          if(intervals == 10) : # Handle transition to DoS phase and end of program in this block
+            if(DoSing) : # End of program
+              print("\nTerminating Session...")
+              signal_handler(signal.SIGINT, 0)
+            else :
+              print("\nTransitioning to DoS phase for", DosDuration, "seconds.") # Start DoS
+              feedbackInterval = DosDuration / 10
+              DoSing = True;
+              intervals = 1
+              transmission = str(0)
+              start = datetime.datetime.now()
+          else :
+            print(intervals * 10, "%... ", end = "", sep = '')
+            sys.stdout.flush()
+            intervals += 1
+            start = datetime.datetime.now()
+
 
 print('Received', repr(data))
--- server.py
+++ server.py
@@ -1,28 +1,66 @@
 import socket
 import datetime
+import sys
+import subprocess
 
 def stats_and_exit():
   end = datetime.datetime.now()
   total = end - start
   print("Finished after: ", total.total_seconds())
+  print("Total normal transmissions: ", switchTrans)
+  print("Total distressed transmissions: ", prevTransmission)
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 9099        # Port to listen on (non-privileged ports are > 1023)
+
+  halfOne = switchTime - start
+  halfTwo = end - switchTime
+  normTPS = int(switchTrans) / halfOne.total_seconds()
+  dosTPS = int(prevTransmission) / halfTwo.total_seconds()
+
+  print("\nNormal transmissions per second: ", int(normTPS))
+  print("Distressed transmissions per second: ", int(dosTPS))
+
+
+
+HOST = '127.0.0.1'  # Default IP to connect to
+PORT = 9099        # Default Port to listen on
+
+if(len(sys.argv) >= 2 and str(sys.argv[1]) == "-h") :
+  print("Usage: python3 server.py [HOST] [PORT]\nDefault HOST is localhost, default PORT is 9099")
+  exit()
+
+if(len(sys.argv) >= 2) :
+  HOST = sys.argv[1]
+  if(len(sys.argv) >= 3) :
+    PORT = sys.argv[2]
+
+prevTransmission = -1;
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((HOST, PORT))
+    sock.bind((HOST, int(PORT)))
     sock.listen()
     conn, addr = sock.accept()
     with conn:
         print('Connected to: ', addr)
+        print("If you must end the program early, use Ctrl-C on the CLIENT program. NOT the server (this)!")
         start = datetime.datetime.now()
+
         while True:
           transmission = conn.recv(1024).decode()
-          if(int(transmission) == -1):
-            break;
+          if(int(transmission) % 10000 == 1) :
+            routeInfo = subprocess.run(["traceroute", HOST]) # Make this into a predefined string
+
+          if(int(transmission) == 0) : # When the client switches to DoS mode
+            switchTime = datetime.datetime.now()
+            switchTrans = prevTransmission
+          else :
+            try:
+              if(int(transmission) != int(prevTransmission) + 1 and int(transmission) != 0) :
+                break;
+            except:
+              break;
+
           transmission = str(int(transmission) + 1)
-          print("From client: ", transmission)
+          prevTransmission = transmission
           conn.sendall(transmission.encode())
 
     stats_and_exit()
-
